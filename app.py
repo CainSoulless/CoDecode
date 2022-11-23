ac6922dbff3529652b50bc459b95f409778df5bf
Rodrigo Hormazabal <rodrigotrickz@gmail.com>
2022-11-22 22:12:00 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,93 @@
+# Flask
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+
+# Security
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# SQLITE3
+import sqlite3
+
+# helpers
+from helpers import login_required
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = sqlite3.connect("database.db", check_same_thread=False)
+db.row_factory = sqlite3.Row
+cursor = db.cursor()
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET"])
+def index():
+    if request.method == "GET":
+        return render_template("/index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        cursor.execute("SELECT username FROM users WHERE username = ?;", (username,))
+        user_exist = cursor.fetchone()
+
+        if not username or not password or user_exist:
+            return "error"
+        
+        cursor.execute("INSERT INTO users (username, hash) VALUES (?, ?);", (username, generate_password_hash(password)))
+        db.commit()
+        
+        return render_template("home.html")
+
+
+@app.route("/", methods=["POST", "GET"])
+@login_required
+def home():
+    return render_template("home.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            return "Insert valid username and/or password"
+
+        cursor.execute("SELECT * FROM users WHERE username = ?;", (username,))
+        row = cursor.fetchone()
+
+        if not check_password_hash(row["hash"], password):
+            return "Invalid username or password"
+
+        session["user_id"] = row["id"]
+
+        return render_template("home.html")
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    session.clear()
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
